b4da9b5b37c860babcfac5cb2270674a6e8a56db
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-03-11 11:06:42 -0600

    rename range.rs to bin.rs

--- rust/bambam-core/src/model/destination/mod.rs
+++ rust/bambam-core/src/model/destination/mod.rs
@@ -1,8 +1,8 @@
+mod bin;
 mod error;
 mod filter;
 pub mod iter;
-mod range;
 
+pub use bin::{BinInterval, BinningConfig};
 pub use error::DestinationError;
 pub use filter::{DestinationFilter, DestinationPredicate};
-pub use range::{BinInterval, BinningConfig};
