96af8236e5fa7f9dff3253264e8edef99e48a4a3
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-04-16 15:42:58 -0600

    prefer Feature "id" field for ZoneId data source

--- rust/bambam-gtfs-flex/src/util/zone/lookup_config.rs
+++ rust/bambam-gtfs-flex/src/util/zone/lookup_config.rs
@@ -5,13 +5,8 @@ pub struct ZoneLookupConfig {
     /// a GeoJSON collection of zone records
     pub zone_record_input_file: String,
     /// optional column name for ZoneId values stored in the zonal GeoJSON input.
-    /// if not provided, "zone_id" will be used.
-    #[serde(default = "default_zone_id_column")]
-    pub zone_id_column: String,
+    /// if not provided, "id" will be used.
+    pub zone_id_property: Option<String>,
     /// geometries for zones
     pub zone_geometry_input_file: String,
-}
-
-fn default_zone_id_column() -> String {
-    "zone_id".to_string()
 }
