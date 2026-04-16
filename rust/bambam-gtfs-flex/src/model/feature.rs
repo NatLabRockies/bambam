d03e4f82726ccf517124d6a26f57cacac706cb84
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-04-16 16:05:17 -0600

    should be signed int

--- rust/bambam-gtfs-flex/src/model/feature.rs
+++ rust/bambam-gtfs-flex/src/model/feature.rs
@@ -9,7 +9,6 @@ pub mod fieldname {
 pub mod variable {
     //! the configuration for state variables in GTFS-Flex routing
 
-    use ordered_float::OrderedFloat;
     use routee_compass_core::model::state::{CustomVariableConfig, StateVariableConfig};
 
     /// stores a zone id in a state variable
@@ -39,8 +38,6 @@ pub mod variable {
 
     /// empty value is "-1" for categoricals mapped to real numbers
     pub fn empty() -> CustomVariableConfig {
-        CustomVariableConfig::FloatingPoint {
-            initial: OrderedFloat(-1.0),
-        }
+        CustomVariableConfig::SignedInteger { initial: -1 }
     }
 }
