c0067875b94aefd9553e31f9b15b08144ea49584
admrtin <adammichaelm.p@gmail.com>
2026-07-13 12:08:03 -0600

    WCI score unit tests, gitignore fix

--- rust/bambam-osm/src/app/network/wci/compute_wci.rs
+++ rust/bambam-osm/src/app/network/wci/compute_wci.rs
@@ -84,24 +84,156 @@ pub fn compute_wci(
 
 #[cfg(test)]
 mod test {
+    use super::compute_wci;
+    use crate::{
+        app::network::{
+            common::way_rtree_entry::WayRTreeEntry,
+            wci::{compute_wci::WCIComponentScores, MAX_WCI_SCORE, MIN_WCI_SCORE},
+        },
+        model::osm::graph::{OsmNodeDataSerializable, OsmWayDataSerializable},
+    };
+    use rstar::RTree;
+    use serde_json;
 
-    #[test]
-    fn test_max_wci() {
-        todo!();
-    }
+    /// Unwalkable highway gives the minimum WCI score
     #[test]
     fn test_min_wci() {
-        // a way has min wci if it has no place to walk (e.g., a primary highway).
-        // inject a fictional way with the appropriate params
-        todo!();
+        let way: OsmWayDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 42,
+            "src_vertex_id": 0,
+            "dst_vertex_id": 1,
+            "highway": "motorway",
+            "maxspeed": "65 mph",
+            "linestring": "LINESTRING (-105.170016 39.773648, -105.165381 39.774176)",
+            "length_meters": 400.0
+        }"#,
+        )
+        .unwrap();
+
+        let src_vertex: OsmNodeDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 0,
+            "x": -105.170016,
+            "y": 39.773648
+        }"#,
+        )
+        .unwrap();
+
+        let entry = WayRTreeEntry::new(way).unwrap();
+        let rtree: RTree<WayRTreeEntry> = RTree::new(); // just need this to pass into wci, not using it.
+
+        let score: WCIComponentScores = compute_wci(&rtree, &entry, &src_vertex);
+        assert_eq!(score.total_score, MIN_WCI_SCORE);
+    }
+
+    /// An isolated footway gives the max WCI score.
+    #[test]
+    fn test_max_wci() {
+        let way: OsmWayDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 42,
+            "src_vertex_id": 0,
+            "dst_vertex_id": 1,
+            "highway": "footway",
+            "footway": "alley",
+            "maxspeed": "",
+            "linestring": "LINESTRING (-105.170016 39.773648, -105.165381 39.774176)",
+            "length_meters": 400.0
+        }"#,
+        )
+        .unwrap();
+
+        let src_vertex: OsmNodeDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 0,
+            "x": -105.170016,
+            "y": 39.773648
+        }"#,
+        )
+        .unwrap();
+
+        let entry = WayRTreeEntry::new(way).unwrap();
+        let rtree: RTree<WayRTreeEntry> = RTree::new(); // just need this to pass into wci, not using it.
+
+        let score: WCIComponentScores = compute_wci(&rtree, &entry, &src_vertex);
+        assert_eq!(score.total_score, MAX_WCI_SCORE);
     }
     #[test]
     fn test_positive_wci() {
-        todo!();
+        // a residential roadway with speed limit 25mph, a shared-lane
+        // cycleway, and a stop sign at the source node
+        let way: OsmWayDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 42,
+            "src_vertex_id": 0,
+            "dst_vertex_id": 1,
+            "highway": "residential",
+            "cycleway": "shared_lane",
+            "maxspeed": "25 mph",
+            "linestring": "LINESTRING (-105.170016 39.773648, -105.165381 39.774176)",
+            "length_meters": 400.0
+        }"#,
+        )
+        .unwrap();
+
+        let src_vertex: OsmNodeDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 0,
+            "x": -105.170016,
+            "y": 39.773648,
+            "highway": "stop"
+        }"#,
+        )
+        .unwrap();
+
+        let entry = WayRTreeEntry::new(way).unwrap();
+        let rtree: RTree<WayRTreeEntry> = RTree::new(); // just need this to pass into wci, not using it.
+
+        // compute wci for the residential highway with nearby sidewalk
+        let score: WCIComponentScores = compute_wci(&rtree, &entry, &src_vertex);
+        assert_eq!(score.traffic_speed_score, Some(2));
+        assert_eq!(score.traffic_signal_score, Some(1));
+        assert_eq!(score.cycle_score, Some(0));
+        assert_eq!(score.walk_score, Some(-2));
+        assert!(score.total_score > 0)
     }
 
     #[test]
     fn test_negative_wci() {
-        todo!();
+        // a residential highway with speed limit 45 mph and a stop sign at the source node
+        let way: OsmWayDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 42,
+            "src_vertex_id": 0,
+            "dst_vertex_id": 1,
+            "highway": "residential",
+            "maxspeed": "45 mph",
+            "linestring": "LINESTRING (-105.170016 39.773648, -105.165381 39.774176)",
+            "length_meters": 400.0
+        }"#,
+        )
+        .unwrap();
+
+        let src_vertex: OsmNodeDataSerializable = serde_json::from_str(
+            r#"{
+            "osmid": 0,
+            "x": -105.170016,
+            "y": 39.773648,
+            "highway": "stop"
+        }"#,
+        )
+        .unwrap();
+
+        let entry = WayRTreeEntry::new(way).unwrap();
+        let rtree: RTree<WayRTreeEntry> = RTree::new(); // just need this to pass into wci, not using it.
+
+        // compute wci for the residential highway with nearby sidewalk
+        let score: WCIComponentScores = compute_wci(&rtree, &entry, &src_vertex);
+        assert_eq!(score.traffic_speed_score, Some(-1));
+        assert_eq!(score.traffic_signal_score, Some(1));
+        assert_eq!(score.cycle_score, Some(-2));
+        assert_eq!(score.walk_score, Some(-2));
+        assert!(score.total_score < 0)
     }
 }
