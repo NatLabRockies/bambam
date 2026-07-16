561b25bdff03108d27cb989c8213ed09f502818b
Rob Fitzgerald <robert.fitzgerald@nrel.gov>
2026-07-15 19:44:57 -0600

    fmt

--- rust/bambam-gbfs/src/app/download/ops.rs
+++ rust/bambam-gbfs/src/app/download/ops.rs
@@ -1,7 +1,6 @@
 use reqwest::{Client, IntoUrl};
 use serde::de::DeserializeOwned;
 
-
 /// helper function for running a client HTTP GET call to retrieve a JSON object.
 pub async fn retrieve_file<'a, 'b, T: DeserializeOwned, U: IntoUrl>(
     client: &'b Client,
